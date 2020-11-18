77ab9f268a2b95eb90817e124d9e920e0b459253
Anatoly <anatoly.selivanov1@gmail.com>
2020-11-18 12:19:51 +0300

    Start

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Maksim</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Evgeniy</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Sergey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Tolya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Tatyana</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>how are you?</div>
+                <div className={s.message}>Hello my friend! How are you?</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import n from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className={n.nav}>
-        <div>
-            <a href="/profile">Profile</a>
+    return (
+    <nav className={n.nav}>
+        <div className={n.item}>
+            <NavLink to="/profile" activeClassName={n.activeLink}>Profile</NavLink>
         </div>
-        <div>
-            <a href="/dialogs">Message</a>
+        <div className={n.item}>
+            <NavLink to="/dialogs" activeClassName={n.activeLink}>Message</NavLink>
         </div>
-        <div>
-            <a href="/news">News</a>
+        <div className={n.item}>
+            <NavLink to="/news" activeClassName={n.activeLink}>News</NavLink>
         </div>
-        <div>
-            <a href="/music">Music</a>
+        <div className={n.item}>
+            <NavLink to="/music" activeClassName={n.activeLink}>Music</NavLink>
         </div>
-        <div>
-            <a href="/settings">Settings</a>
+        <div className={n.item}>
+            <NavLink to="/settings" activeClassName={n.activeLink}>Settings</NavLink>
         </div>
     </nav>
+    )
 }
 
 export default Navbar;
